38ae18f91282f255187bf760f16d70b7b684573e
Vitor Silveira <vitor.silveiravtss@gmail.com>
2023-11-28 17:14:35 -0300

    backend relacionado aos professores feito

--- api/controllers/professoresController.js
+++ api/controllers/professoresController.js
@@ -12,4 +12,46 @@ export const getProfessores = (_, res) => {
     console.log(data)
     return res.status(200).json(data);
   });
+};
+
+export const addProfessor = (req, res) => {
+  const query = "INSERT INTO professor(`nome`, `email`, `cpf`) VALUES(?)"
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.cpf,
+  ];
+
+  db.query(query, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Professor criado com sucesso.")
+  });
+};
+
+export const updateProfessor = (req, res) => {
+  const query = "UPDATE professor SET `nome` = ?, `email` = ?, `cpf` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.cpf,
+  ];
+
+  db.query(query, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Professor atualizado com sucesso.")
+  })
+}
+
+export const deleteProfessor = (req, res) => {
+  const query = "DELETE FROM professor WHERE `id` = ?";
+
+  db.query(query, [req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Professor deletado com sucesso.")
+  })
 }
--- api/routes/professores.js
+++ api/routes/professores.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getProfessores } from "../controllers/professoresController.js"
+import { getProfessores, addProfessor, updateProfessor, deleteProfessor } from "../controllers/professoresController.js"
 
 const professoresRouter = express.Router()
 
 professoresRouter.get("/", getProfessores)
+
+professoresRouter.post("/", addProfessor)
+
+professoresRouter.put("/:id", updateProfessor)
+
+professoresRouter.delete("/:id", deleteProfessor)
 
 export default professoresRouter
